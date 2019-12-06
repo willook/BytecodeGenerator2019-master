7a6bab4945b6b5ae3ff2e6009bcdb6e2afcd12e2
willook@naver.com <willook@naver.com>
2019-12-06 20:43:17 +0900

    Add Test.j in gitignore

---
+++ test/test3.c
@@ -0,0 +1,14 @@
+void main() {
+    int d = 3;
+    int b = 1;
+    int c = 6;
+    int f = 4;
+    int h;
+
+    if( c>5)
+        while(c != f) {
+            _print(f);
+            if(d<10)
+                ++  f;
+        }
+}
---
+++ test/test4.c
@@ -0,0 +1,13 @@
+int add(int x, int y) {
+   int z;
+   z = x+y;
+   return z;
+}
+
+void main () {
+   int t = 10;
+   while(! ((t-2)<5)) {
+       _print(add(0,t));
+       --t;
+   }
+}
